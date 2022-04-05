92a4669625b0783d24e9ae78a168d19352d1d085
umitakbas82 <umit.akbas@hotmail.com.tr>
2022-04-06 01:31:15 +0300

    reload

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -10,6 +10,7 @@ export class ProductComponent implements OnInit {
 
   constructor() { }
   title="Product List";
+  filterText!: "";
   products:Product[]=[
     {id:1,name:"Laptop",price:50000,categoryId:1, description:" Asus Zenbook",imageUrl:"https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80"},
     {id:2,name:"Mobile",price:20000,categoryId:2, description:"Mobile Description",imageUrl:"https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"},
